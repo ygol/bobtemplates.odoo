64258891ea0e6d5358b38898c918169142d4c70b
Yves Goldberg <admin@ygol.com>
2022-03-19 11:55:29 +0200

    style(bobtemplates)

--- bobtemplates/itpp/odoo/hooks.py
+++ bobtemplates/itpp/odoo/hooks.py
@@ -4,10 +4,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import ast
+import datetime
 import os
 import re
-import datetime
-
 from subprocess import check_output
 
 from mrbob.bobexceptions import ValidationError
@@ -18,26 +17,33 @@ from pkg_resources import parse_version
 def _dotted_to_camelcased(dotted):
     return "".join([s.capitalize() for s in dotted.split(".")])
 
+
 def _dotted_to_underscored(dotted):
     return dotted.replace(".", "_")
+
 
 def _dotted_to_camelwords(dotted):
     return " ".join([s.capitalize() for s in dotted.split(".")])
 
+
 def _underscored_to_camelcased(underscored):
     return "".join([s.capitalize() for s in underscored.split("_")])
+
 
 def _underscored_to_camelwords(underscored):
     return " ".join([s.capitalize() for s in underscored.split("_")])
 
+
 def _spaced_to_underscored_and_lowered(spaced):
-    return spaced.replace(" ","_").lower()
+    return spaced.replace(" ", "_").lower()
+
 
 def _spaced_to_unspaced_and_lowered(spaced):
-    return spaced.replace(" ","").lower()
+    return spaced.replace(" ", "").lower()
+
 
 def _delete_file(configurator, path):
-    """ remove file and remove it's directories if empty """
+    """remove file and remove it's directories if empty"""
     path = os.path.join(configurator.target_directory, path)
     os.remove(path)
     try:
@@ -45,18 +51,21 @@ def _delete_file(configurator, path):
     except OSError:
         pass
 
+
 def _open_file(configurator, file, mode="r"):
     file_path = os.path.join(configurator.target_directory, file)
     if not os.path.exists(file_path):
         raise ValidationError("{} not found".format(file_path))
     return open(file_path, mode)
 
+
 def _load_manifest(configurator):
     with _open_file(configurator, "__manifest__.py") as f:
         return ast.literal_eval(f.read())
 
+
 def _insert_manifest_item(configurator, key, item):
-    """ Insert an item in the list of an existing manifest key """
+    """Insert an item in the list of an existing manifest key"""
     with _open_file(configurator, "__manifest__.py") as f:
         manifest = f.read()
     if item in ast.literal_eval(manifest).get(key, []):
@@ -68,6 +77,7 @@ def _insert_manifest_item(configurator, key, item):
     with _open_file(configurator, "__manifest__.py", "w") as f:
         f.write(manifest)
 
+
 def _add_in_file_text(configurator, dir_path, to_file, import_string):
     init_path = os.path.join(configurator.target_directory, dir_path, to_file)
     variables = configurator.variables
@@ -75,30 +85,34 @@ def _add_in_file_text(configurator, dir_path, to_file, import_string):
     if os.path.exists(init_path):
         with open(init_path, "U") as f:
             init = f.read()
-            if to_file == 'assets.xml':
+            if to_file == "assets.xml":
                 flag = "w"
                 idx = init.index("</odoo>")
                 import_string = init[:idx] + import_string + "\n" + init[idx:]
     else:
         init = ""
-        os.makedirs(os.path.split(init_path)[0],exist_ok=True)
-        if to_file == 'assets.xml':
-            import_string = (""
-                             "<odoo>\n{3}\n</odoo>\n").format(
-                                                variables["copyright.year"],
-                                                variables["copyright.name"],
-                                                variables["copyright.github"],
-                                                import_string)
-        if to_file == 'ir.model.access.csv':
-            import_string = ("id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink{0}".format(import_string))
+        os.makedirs(os.path.split(init_path)[0], exist_ok=True)
+        if to_file == "assets.xml":
+            import_string = ("" "<odoo>\n{3}\n</odoo>\n").format(
+                variables["copyright.year"],
+                variables["copyright.name"],
+                variables["copyright.github"],
+                import_string,
+            )
+        if to_file == "ir.model.access.csv":
+            import_string = "id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink{}".format(
+                import_string
+            )
 
     if import_string not in init.split("\n"):
         open(init_path, flag).write(import_string)
+
 
 def _rm_suffix(suffix, configurator, path):
     path = os.path.join(configurator.target_directory, path)
     assert path.endswith(suffix)
     os.rename(path, path[: -len(suffix)])
+
 
 #
 # addon hooks
@@ -115,7 +129,8 @@ def pre_render_addon(configurator):
     # [copyright.year] - year of creation
     variables["copyright.year"] = datetime.date.today().year
     # [addon.branch] - name of git-branch
-    variables["addon.branch"] = str(variables["odoo.version"])+ ".0"
+    variables["addon.branch"] = str(variables["odoo.version"]) + ".0"
+
 
 def post_render_addon(configurator):
     category_list = [
@@ -134,7 +149,7 @@ def post_render_addon(configurator):
     variables = configurator.variables
     configurator.target_directory += "/" + variables["addon.technical_name"]
     if variables["addon.dependency"]:
-        for mod in variables["addon.dependency"].split(', '):
+        for mod in variables["addon.dependency"].split(", "):
             _insert_manifest_item(configurator, "depends", mod)
     # Handle icons in depends of technical_category
     _rm_suffix(
@@ -159,6 +174,7 @@ def pre_render_data(configurator):
         os.path.normpath(configurator.target_directory)
     )
 
+
 def post_render_data(configurator):
     variables = configurator.variables
     data_path = "data/{}.xml".format(variables["data.name_underscored"])
@@ -175,6 +191,7 @@ def pre_render_demo(configurator):
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_demo(configurator):
     variables = configurator.variables
@@ -193,6 +210,7 @@ def pre_render_qweb(configurator):
         os.path.normpath(configurator.target_directory)
     )
 
+
 def post_render_qweb(configurator):
     variables = configurator.variables
     qweb_path = "static/src/xml/{}.xml".format(variables["qweb.name_underscored"])
@@ -209,6 +227,7 @@ def pre_render_view(configurator):
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_view(configurator):
     variables = configurator.variables
@@ -236,6 +255,7 @@ def pre_render_model(configurator):
     # model defaults
     #
 
+
 def post_render_model(configurator):
     variables = configurator.variables
     security_path = "security/ir.model.access.csv"
@@ -245,8 +265,14 @@ def post_render_model(configurator):
     import_string = "from . import {}".format(variables["model.name_underscored"])
     _add_in_file_text(configurator, "models", "__init__.py", import_string)
     if variables["model.security"]:
-        import_string = "\naccess_{0},access_{0},model_{0},base.group_user,1,1,1,1".format(variables["model.name_underscored"])
-        _add_in_file_text(configurator, "security", "ir.model.access.csv", import_string)
+        import_string = (
+            "\naccess_{0},access_{0},model_{0},base.group_user,1,1,1,1".format(
+                variables["model.name_underscored"]
+            )
+        )
+        _add_in_file_text(
+            configurator, "security", "ir.model.access.csv", import_string
+        )
         _insert_manifest_item(configurator, "data", security_path)
     # show message if any
     show_message(configurator)
@@ -268,6 +294,7 @@ def pre_render_controller(configurator):
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_controller(configurator):
     variables = configurator.variables
@@ -296,6 +323,7 @@ def pre_render_css(configurator):
         os.path.normpath(configurator.target_directory)
     )
 
+
 def post_render_css(configurator):
     variables = configurator.variables
     script_text = """
@@ -304,14 +332,15 @@ def post_render_css(configurator):
             <link rel="stylesheet" href="/{2}/static/src/css/{0}.css"/>
         </xpath>
     </template>
-    """.format(variables["css.name_underscored"],
-               variables["css.inherit"],
-               variables["addon.name"])
+    """.format(
+        variables["css.name_underscored"],
+        variables["css.inherit"],
+        variables["addon.name"],
+    )
     _add_in_file_text(configurator, "views", "assets.xml", script_text)
     xml_path = "views/assets.xml".format(variables["css.name_underscored"])
     _insert_manifest_item(configurator, "data", xml_path)
     show_message(configurator)
-
 
 
 #
@@ -331,6 +360,7 @@ def pre_render_js(configurator):
         os.path.normpath(configurator.target_directory)
     )
 
+
 def post_render_js(configurator):
     variables = configurator.variables
     script_text = """
@@ -339,9 +369,11 @@ def post_render_js(configurator):
             <script type="text/javascript" src="/{2}/static/src/js/{0}.js"></script>
         </xpath>
     </template>
-    """.format(variables["js.name_underscored"],
-               variables["js.inherit"],
-               variables["addon.name"])
+    """.format(
+        variables["js.name_underscored"],
+        variables["js.inherit"],
+        variables["addon.name"],
+    )
     _add_in_file_text(configurator, "views", "assets.xml", script_text)
     assets_path = "views/assets.xml".format(variables["js.name_underscored"])
     _insert_manifest_item(configurator, "data", assets_path)
@@ -357,13 +389,14 @@ def pre_render_test(configurator):
     variables = configurator.variables
     category = variables["addon.category"]
     if category in ["website", "website_sale"]:
-        variables["test.assets"] =  "website.assets_frontend"
+        variables["test.assets"] = "website.assets_frontend"
     else:
         variables["test.assets"] = "web.assets_backend"
     variables["odoo.version"] = int(variables["addon.version"])
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_test(configurator):
     variables = configurator.variables
@@ -373,9 +406,11 @@ def post_render_test(configurator):
             <script type="text/javascript" src="/{2}/static/src/js/test_{0}.js"></script>
         </xpath>
     </template>
-    """.format(variables["test.name_underscored"],
-               variables["test.assets"],
-               variables["addon.name"])
+    """.format(
+        variables["test.name_underscored"],
+        variables["test.assets"],
+        variables["addon.name"],
+    )
     _add_in_file_text(configurator, "views", "assets.xml", script_text)
     import_string = "from . import test_{}".format(variables["test.name_underscored"])
     _add_in_file_text(configurator, "tests", "__init__.py", import_string)
@@ -388,6 +423,7 @@ def post_render_test(configurator):
         _delete_file(configurator, assets_path)
     # show message if any
     show_message(configurator)
+
 
 #
 # wizard hooks
@@ -408,6 +444,7 @@ def pre_render_wizard(configurator):
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_wizard(configurator):
     variables = configurator.variables
--- bobtemplates/ygol/odoo/hooks.py
+++ bobtemplates/ygol/odoo/hooks.py
@@ -5,10 +5,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import ast
+import datetime
 import os
 import re
-import datetime
-
 from subprocess import check_output
 
 from mrbob.bobexceptions import ValidationError
@@ -19,26 +18,33 @@ from pkg_resources import parse_version
 def _dotted_to_camelcased(dotted):
     return "".join([s.capitalize() for s in dotted.split(".")])
 
+
 def _dotted_to_underscored(dotted):
     return dotted.replace(".", "_")
+
 
 def _dotted_to_camelwords(dotted):
     return " ".join([s.capitalize() for s in dotted.split(".")])
 
+
 def _underscored_to_camelcased(underscored):
     return "".join([s.capitalize() for s in underscored.split("_")])
+
 
 def _underscored_to_camelwords(underscored):
     return " ".join([s.capitalize() for s in underscored.split("_")])
 
+
 def _spaced_to_underscored_and_lowered(spaced):
-    return spaced.replace(" ","_").lower()
+    return spaced.replace(" ", "_").lower()
+
 
 def _spaced_to_unspaced_and_lowered(spaced):
-    return spaced.replace(" ","").lower()
+    return spaced.replace(" ", "").lower()
+
 
 def _delete_file(configurator, path):
-    """ remove file and remove it's directories if empty """
+    """remove file and remove it's directories if empty"""
     path = os.path.join(configurator.target_directory, path)
     os.remove(path)
     try:
@@ -46,18 +52,21 @@ def _delete_file(configurator, path):
     except OSError:
         pass
 
+
 def _open_file(configurator, file, mode="r"):
     file_path = os.path.join(configurator.target_directory, file)
     if not os.path.exists(file_path):
         raise ValidationError("{} not found".format(file_path))
     return open(file_path, mode)
 
+
 def _load_manifest(configurator):
     with _open_file(configurator, "__manifest__.py") as f:
         return ast.literal_eval(f.read())
 
+
 def _insert_manifest_item(configurator, key, item):
-    """ Insert an item in the list of an existing manifest key """
+    """Insert an item in the list of an existing manifest key"""
     with _open_file(configurator, "__manifest__.py") as f:
         manifest = f.read()
     if item in ast.literal_eval(manifest).get(key, []):
@@ -69,6 +78,7 @@ def _insert_manifest_item(configurator, key, item):
     with _open_file(configurator, "__manifest__.py", "w") as f:
         f.write(manifest)
 
+
 def _add_in_file_text(configurator, dir_path, to_file, import_string):
     init_path = os.path.join(configurator.target_directory, dir_path, to_file)
     variables = configurator.variables
@@ -76,30 +86,34 @@ def _add_in_file_text(configurator, dir_path, to_file, import_string):
     if os.path.exists(init_path):
         with open(init_path, "U") as f:
             init = f.read()
-            if to_file == 'assets.xml':
+            if to_file == "assets.xml":
                 flag = "w"
                 idx = init.index("</odoo>")
                 import_string = init[:idx] + import_string + "\n" + init[idx:]
     else:
         init = ""
-        os.makedirs(os.path.split(init_path)[0],exist_ok=True)
-        if to_file == 'assets.xml':
-            import_string = (""
-                             "<odoo>\n{3}\n</odoo>\n").format(
-                                                variables["copyright.year"],
-                                                variables["copyright.name"],
-                                                variables["copyright.github"],
-                                                import_string)
-        if to_file == 'ir.model.access.csv':
-            import_string = ("id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink{0}".format(import_string))
+        os.makedirs(os.path.split(init_path)[0], exist_ok=True)
+        if to_file == "assets.xml":
+            import_string = ("" "<odoo>\n{3}\n</odoo>\n").format(
+                variables["copyright.year"],
+                variables["copyright.name"],
+                variables["copyright.github"],
+                import_string,
+            )
+        if to_file == "ir.model.access.csv":
+            import_string = "id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink{}".format(
+                import_string
+            )
 
     if import_string not in init.split("\n"):
         open(init_path, flag).write(import_string)
+
 
 def _rm_suffix(suffix, configurator, path):
     path = os.path.join(configurator.target_directory, path)
     assert path.endswith(suffix)
     os.rename(path, path[: -len(suffix)])
+
 
 #
 # addon hooks
@@ -116,7 +130,8 @@ def pre_render_addon(configurator):
     # [copyright.year] - year of creation
     variables["copyright.year"] = datetime.date.today().year
     # [addon.branch] - name of git-branch
-    variables["addon.branch"] = str(variables["odoo.version"])+ ".0"
+    variables["addon.branch"] = str(variables["odoo.version"]) + ".0"
+
 
 def post_render_addon(configurator):
     category_list = [
@@ -135,7 +150,7 @@ def post_render_addon(configurator):
     variables = configurator.variables
     configurator.target_directory += "/" + variables["addon.technical_name"]
     if variables["addon.dependency"]:
-        for mod in variables["addon.dependency"].split(', '):
+        for mod in variables["addon.dependency"].split(", "):
             _insert_manifest_item(configurator, "depends", mod)
     # Handle icons in depends of technical_category
     _rm_suffix(
@@ -160,6 +175,7 @@ def pre_render_data(configurator):
         os.path.normpath(configurator.target_directory)
     )
 
+
 def post_render_data(configurator):
     variables = configurator.variables
     data_path = "data/{}.xml".format(variables["data.name_underscored"])
@@ -176,6 +192,7 @@ def pre_render_demo(configurator):
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_demo(configurator):
     variables = configurator.variables
@@ -194,6 +211,7 @@ def pre_render_qweb(configurator):
         os.path.normpath(configurator.target_directory)
     )
 
+
 def post_render_qweb(configurator):
     variables = configurator.variables
     qweb_path = "static/src/xml/{}.xml".format(variables["qweb.name_underscored"])
@@ -210,6 +228,7 @@ def pre_render_view(configurator):
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_view(configurator):
     variables = configurator.variables
@@ -237,6 +256,7 @@ def pre_render_model(configurator):
     # model defaults
     #
 
+
 def post_render_model(configurator):
     variables = configurator.variables
     security_path = "security/ir.model.access.csv"
@@ -246,8 +266,14 @@ def post_render_model(configurator):
     import_string = "from . import {}".format(variables["model.name_underscored"])
     _add_in_file_text(configurator, "models", "__init__.py", import_string)
     if variables["model.security"]:
-        import_string = "\naccess_{0},access_{0},model_{0},base.group_user,1,1,1,1".format(variables["model.name_underscored"])
-        _add_in_file_text(configurator, "security", "ir.model.access.csv", import_string)
+        import_string = (
+            "\naccess_{0},access_{0},model_{0},base.group_user,1,1,1,1".format(
+                variables["model.name_underscored"]
+            )
+        )
+        _add_in_file_text(
+            configurator, "security", "ir.model.access.csv", import_string
+        )
         _insert_manifest_item(configurator, "data", security_path)
     # show message if any
     show_message(configurator)
@@ -269,6 +295,7 @@ def pre_render_controller(configurator):
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_controller(configurator):
     variables = configurator.variables
@@ -297,6 +324,7 @@ def pre_render_css(configurator):
         os.path.normpath(configurator.target_directory)
     )
 
+
 def post_render_css(configurator):
     variables = configurator.variables
     script_text = """
@@ -305,14 +333,15 @@ def post_render_css(configurator):
             <link rel="stylesheet" href="/{2}/static/src/css/{0}.css"/>
         </xpath>
     </template>
-    """.format(variables["css.name_underscored"],
-               variables["css.inherit"],
-               variables["addon.name"])
+    """.format(
+        variables["css.name_underscored"],
+        variables["css.inherit"],
+        variables["addon.name"],
+    )
     _add_in_file_text(configurator, "views", "assets.xml", script_text)
     xml_path = "views/assets.xml".format(variables["css.name_underscored"])
     _insert_manifest_item(configurator, "data", xml_path)
     show_message(configurator)
-
 
 
 #
@@ -332,6 +361,7 @@ def pre_render_js(configurator):
         os.path.normpath(configurator.target_directory)
     )
 
+
 def post_render_js(configurator):
     variables = configurator.variables
     script_text = """
@@ -340,9 +370,11 @@ def post_render_js(configurator):
             <script type="text/javascript" src="/{2}/static/src/js/{0}.js"></script>
         </xpath>
     </template>
-    """.format(variables["js.name_underscored"],
-               variables["js.inherit"],
-               variables["addon.name"])
+    """.format(
+        variables["js.name_underscored"],
+        variables["js.inherit"],
+        variables["addon.name"],
+    )
     _add_in_file_text(configurator, "views", "assets.xml", script_text)
     assets_path = "views/assets.xml".format(variables["js.name_underscored"])
     _insert_manifest_item(configurator, "data", assets_path)
@@ -358,13 +390,14 @@ def pre_render_test(configurator):
     variables = configurator.variables
     category = variables["addon.category"]
     if category in ["website", "website_sale"]:
-        variables["test.assets"] =  "website.assets_frontend"
+        variables["test.assets"] = "website.assets_frontend"
     else:
         variables["test.assets"] = "web.assets_backend"
     variables["odoo.version"] = int(variables["addon.version"])
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_test(configurator):
     variables = configurator.variables
@@ -374,9 +407,11 @@ def post_render_test(configurator):
             <script type="text/javascript" src="/{2}/static/src/js/test_{0}.js"></script>
         </xpath>
     </template>
-    """.format(variables["test.name_underscored"],
-               variables["test.assets"],
-               variables["addon.name"])
+    """.format(
+        variables["test.name_underscored"],
+        variables["test.assets"],
+        variables["addon.name"],
+    )
     _add_in_file_text(configurator, "views", "assets.xml", script_text)
     import_string = "from . import test_{}".format(variables["test.name_underscored"])
     _add_in_file_text(configurator, "tests", "__init__.py", import_string)
@@ -389,6 +424,7 @@ def post_render_test(configurator):
         _delete_file(configurator, assets_path)
     # show message if any
     show_message(configurator)
+
 
 #
 # wizard hooks
@@ -409,6 +445,7 @@ def pre_render_wizard(configurator):
     variables["addon.name"] = os.path.basename(
         os.path.normpath(configurator.target_directory)
     )
+
 
 def post_render_wizard(configurator):
     variables = configurator.variables
